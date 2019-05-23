93596d55861248bd2e1c8073a5188425ceda7a3d
dexy58 <40712374+dexy58@users.noreply.github.com>
2019-05-23 11:58:54 +0200

    added relay subscription

--- fs/init.js
+++ fs/init.js
@@ -1,7 +1,17 @@
 load('api_config.js');
-load('api_dht.js');
+load('api_events.js');
+load('api_gpio.js');
 load('api_mqtt.js');
+load('api_net.js');
+load('api_sys.js');
 load('api_timer.js');
+load('api_dht.js');
+
+let relay_pin = 0;  // Sonoff relay pin
+// init hardware
+GPIO.set_mode(relay_pin, GPIO.MODE_OUTPUT);
+GPIO.write(relay_pin, 0);  // default to off
+
 
 let deviceIDCustom ='01';
 let topicTemp ='home/device' + deviceIDCustom +'/temperature';
@@ -10,7 +20,20 @@ let topicHum ='home/device' + deviceIDCustom +'/humidity';
 let topic = '/devices/' + Cfg.get('device.id') + '/events';
 let dht = DHT.create(14, DHT.AM2302); //14 is the pin which is connected from DHT22 to ESP8266
 
-Timer.set(2000, true, function() {
+MQTT.sub('home/device01/switchLight', function(conn, topic, msg) {
+  print('Topic:', topic, 'message:', msg);
+  if(msg === 'ON'){
+	  print('Switch is ON');
+	  GPIO.write(relay_pin,1);
+  }
+  if(msg === 'OFF'){
+	  print('Switch is OFF');
+	  GPIO.write(relay_pin, 0);
+  }
+  //radi
+}, null);
+
+Timer.set(3000, true, function() {
   let messageTemp = JSON.stringify(dht.getTemp());
   let okTemp = MQTT.pub(topicTemp, messageTemp, 2); //use qos=2
   print('Published:', okTemp, topicTemp, '->', messageTemp);
@@ -20,4 +43,5 @@ Timer.set(2000, true, function() {
   //let msg = JSON.stringify({ t: dht.getTemp(), h: dht.getHumidity() });
   //let ok = MQTT.pub(topic, msg, 1);
   //print(ok, msg);
+  print ('Device id: ', Cfg.get('device.id'));
 }, null);
