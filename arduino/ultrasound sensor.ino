59940a0944852e7039b1a5d3afea9eeed5884a69
dexy58 <40712374+dexy58@users.noreply.github.com>
2019-06-13 13:19:01 +0200

    made it for consistent

--- arduino/ultrasound sensor.ino
+++ arduino/ultrasound sensor.ino
@@ -1,3 +1,7 @@
+#include <Servo.h> // including servo library.
+
+Servo servo_1; // Giving name to servo.
+
 int trigPin = 15;    // Trigger
 int echoPin = 2;    // Echo
 long duration, cm, inches;
@@ -13,26 +17,33 @@ void setup() {
   //Define inputs and outputs
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  servo_1.attach(16); // Attaching Servo to 16
 }
  
 void loop() {
+  int returnBack = 180;
   if(flag == true){
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(5);
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    pinMode(echoPin, INPUT);
-    duration = pulseIn(echoPin, HIGH);
-    cm = (duration/2) / 29.1;
     for(int i=0;i<180;i++){
+      servo_1.write(i);
+      digitalWrite(trigPin, LOW);
+      delayMicroseconds(5);
+      digitalWrite(trigPin, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin, LOW);
+      pinMode(echoPin, INPUT);
+      duration = pulseIn(echoPin, HIGH);
+      cm = (duration/2) / 29.1;
       distanceArray[i] = cm;
+      delay(20);
     }
     flag = false;
     delay(1000);
   }
   else{
+    servo_1.write(0);
+    delay(250);
     for(int i=0;i<180;i++){
+      servo_1.write(i);
       digitalWrite(trigPin, LOW);
       delayMicroseconds(5);
       digitalWrite(trigPin, HIGH);
@@ -45,20 +56,26 @@ void loop() {
       Serial.println(cm);
       Serial.print("Izmjereno prije: ");
       Serial.println(distanceArray[i]);
-      if(abs(cm-distanceArray[i])>distanceArray[i]*0.3 && counter==0){
+      if(abs(cm-distanceArray[i])>distanceArray[i]*0.4 && counter>=0 && counter<=1){
         Serial.println("Potencijalni provalnik");
         counter++;
       }
-      else if(abs(cm-distanceArray[i])>distanceArray[i]*0.3 && counter>1){
+      else if(abs(cm-distanceArray[i])>distanceArray[i]*0.4 && counter>1){
         Serial.println("Provalnik");
         counter=0;
+        flag = true;
+        returnBack = i;
+        break;
       }
       else{
         distanceArray[i]=cm;
         counter=0;
       }     
-      delay(250);
+      delay(150);
     }
-    delay(1000);
   }
+  for(int returnNow = returnBack; returnNow>=1; returnNow-=1){                                
+    servo_1.write(returnNow);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
 }
