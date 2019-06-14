56bb749291c159b2b82f1911343ef3038586fe2b
dexy58 <40712374+dexy58@users.noreply.github.com>
2019-06-14 09:36:58 +0200

    fixed a lot of things

--- arduino/ultrasound sensor.ino
+++ arduino/ultrasound sensor.ino
@@ -21,8 +21,9 @@ void setup() {
 }
  
 void loop() {
-  int returnBack = 180;
+  int returnBack = 0;
   if(flag == true){
+    counter = 0;
     for(int i=0;i<180;i++){
       servo_1.write(i);
       digitalWrite(trigPin, LOW);
@@ -36,11 +37,11 @@ void loop() {
       distanceArray[i] = cm;
       delay(20);
     }
+    returnBack = 180;
     flag = false;
     delay(1000);
   }
   else{
-    counter = 0;
     servo_1.write(0);
     delay(250);
     for(int i=0;i<180;i++){
@@ -72,11 +73,44 @@ void loop() {
         distanceArray[i]=cm;
         counter=0;
       }     
-      delay(100);
+      delay(50);
+    }
+    if(flag == false){
+      for(int i=179;i>=0;i--){
+        servo_1.write(i);
+        digitalWrite(trigPin, LOW);
+        delayMicroseconds(5);
+        digitalWrite(trigPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(trigPin, LOW);
+        pinMode(echoPin, INPUT);
+        duration = pulseIn(echoPin, HIGH);
+        cm = (duration/2) / 29.1;
+        Serial.print("Izmjereno sada: ");
+        Serial.println(cm);
+        Serial.print("Izmjereno prije: ");
+        Serial.println(distanceArray[i]);
+        if(abs(cm-distanceArray[i])>distanceArray[i]*0.4 && counter>=0 && counter<=4){
+          Serial.println("Potencijalni provalnik");
+          counter++;
+        }
+        else if(abs(cm-distanceArray[i])>distanceArray[i]*0.4 && counter>4){
+          Serial.println("Provalnik");
+          counter=0;
+          flag = true;
+          returnBack = i;
+          break;
+        }
+        else{
+          distanceArray[i]=cm;
+          counter=0;
+        }     
+        delay(50);      
+      }   
     }
   }
-  for(int returnNow = returnBack; returnNow>=1; returnNow-=1){                                
-    servo_1.write(returnNow);              // tell servo to go to position in variable 'pos' 
-    delay(10);                       // waits 15ms for the servo to reach the position 
+  for(int returnNow = returnBack; returnNow>=0; returnNow--){                                
+    servo_1.write(returnNow);
+    delay(10);
   } 
 }
