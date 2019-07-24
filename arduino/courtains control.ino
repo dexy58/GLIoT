4af62100af9f2fbd9f5751449e0b16e06e07e8ff
dexy58 <40712374+dexy58@users.noreply.github.com>
2019-07-24 08:13:41 +0200

    added connection to AP and broker. Added MQTT.

--- arduino/courtains control.ino
+++ arduino/courtains control.ino
@@ -1,4 +1,9 @@
-//motor A connected between A01 and A02
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+void callback(char* topic, byte* payload, unsigned int length);
 
 int STBY = 16; //standby
 
@@ -8,24 +13,55 @@ int AIN1 = 12; //Direction
 int AIN2 = 13; //Direction
 
 void setup(){
+   WiFi.begin("SmartHomeAP", "smarthome");
+   Serial.print("Connecting");
+   while (WiFi.status() != WL_CONNECTED)
+   {
+     delay(500);
+     Serial.print(".");
+   }
+   Serial.println();
+   Serial.print("Connected, IP address: ");
+   Serial.println(WiFi.localIP());
+   client.setServer("192.168.1.162", 1883);
+   client.setCallback(callback);
+   while (!client.connected()) {
+     Serial.println("Connecting to MQTT...");
+     if(client.connect("device01", "openhabian", "openhabian" )) {
+       Serial.println("connected");  
+     }else {
+       Serial.print("failed with state ");
+       Serial.print(client.state());
+       delay(2000); 
+     }
+   }
+   client.subscribe("home/device01/switchCurtains");
    pinMode(STBY, OUTPUT);
    pinMode(PWMA, OUTPUT);
    pinMode(AIN1, OUTPUT);
    pinMode(AIN2, OUTPUT);
 }
 
+void callback(char* topic, byte* payload, unsigned int length) { 
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  if (strcmp(topic,"home/device01/switchCurtains")==0){
+    if (!strncmp((char *)payload, "ON", length)) {
+      Serial.print("ON");
+      move(1,200,0);
+      delay(2500);
+      stop();
+    }
+    else{
+      Serial.print("OFF");
+      move(1,200,1);
+      delay(2500);
+      stop();
+    }
+  }
+}
+
 void loop(){
-   move(1, 255, 1); //motor 1, full speed, left
-
-   delay(1000); //go for 1 second
-   stop(); //stop
-   delay(250); //hold for 250ms until move again
-
-   move(1, 128, 0); //motor 1, half speed, right
-
-   delay(1000);
-   stop();
-   delay(250);
 }
 
 void move(int motor, int speed, int direction){
