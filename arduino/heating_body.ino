b6b4c49fa5ca5581b0f77c818cf64fc708671b19
dexy58 <40712374+dexy58@users.noreply.github.com>
2019-07-29 11:12:41 +0200

    fixed analogWrite values

--- arduino/heating_body.ino
+++ arduino/heating_body.ino
@@ -43,13 +43,13 @@ void loop() {
   T = (double)Temperature/340+36.53; //temperature formula
   Serial.print(" T: "); Serial.println(T);
   if(T<=15){
-    analogWrite(heating_body, 1023);
+    analogWrite(heating_body, 255);
   }
   else if(T>15 && T<=19){
-    analogWrite(heating_body, 613);
+    analogWrite(heating_body, 127);
   }
   else if(T>19 && T<=22){
-    analogWrite(heating_body, 204);
+    analogWrite(heating_body, 64);
   }
   else if(T>22){
     analogWrite(heating_body, 0);
