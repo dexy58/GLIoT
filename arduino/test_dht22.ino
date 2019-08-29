0962638d4da6d8b6defb97006c09392d9dfc5ec6
dexy58 <40712374+dexy58@users.noreply.github.com>
2019-08-29 08:40:20 +0200

    added max and min temperature interval

--- arduino/test_dht22.ino
+++ arduino/test_dht22.ino
@@ -17,6 +17,14 @@ char humidityChar[10];
 
 const char TEMPERATURE_PUB[40] = "home/device03/temperature";
 const char HUMIDITY_PUB[40] = "home/device03/humidity";
+const char FAN_MAX_TEMP_SUB[40]="home/device02/tempMaxFan";
+const char FAN_MIN_TEMP_SUB[40]="home/device02/tempMinFan";
+const int FAN_MAX_TEMP = 26;
+const int FAN_MIN_TEMP = 23;
+
+float fanMaxTemp = FAN_MAX_TEMP;
+float fanMinTemp = FAN_MIN_TEMP;
+float percentage;
 
 void setup() {
   delay(5000);
@@ -48,10 +56,31 @@ void setup() {
     }
   }
   dht.setup(2, DHTesp::DHT22);
+  client.subscribe(FAN_MAX_TEMP_SUB);
+  client.subscribe(FAN_MIN_TEMP_SUB);
 }
 
-void callback(char* topic, byte* payload, unsigned int length) { 
-  
+void callback(char* topic, byte* payload, unsigned int length) {
+  if (strcmp(topic,FAN_MAX_TEMP_SUB)==0){
+    char payload_string[length + 1];
+    memcpy(payload_string, payload, length);
+    payload_string[length] = '\0';
+    int result = atoi(payload_string);
+    percentage = (float)result/100.0;
+    fanMaxTemp = FAN_MAX_TEMP+2*percentage;
+    Serial.print("Current max temp: ");
+    Serial.println(fanMaxTemp);
+  }
+  else if(strcmp(topic,FAN_MIN_TEMP_SUB)==0){
+    char payload_string[length + 1];
+    memcpy(payload_string, payload, length);
+    payload_string[length] = '\0';
+    int result = atoi(payload_string);
+    percentage = (float)result/100.0;
+    fanMinTemp = FAN_MIN_TEMP+2*percentage;
+    Serial.print("Current min temp: ");
+    Serial.println(fanMinTemp);
+  }
 }
 
 void loop() {
@@ -99,9 +128,9 @@ void loop() {
       client.publish(HUMIDITY_PUB, "-99");
     }
     counterDHT22++;
-    Serial.print("Get status string: ");
-    Serial.println(dht.getStatusString());
-    Serial.println(counterDHT22);
+    //Serial.print("Get status string: ");
+    //Serial.println(dht.getStatusString());
+    //Serial.println(counterDHT22);
     delay(250);
   }
 }
